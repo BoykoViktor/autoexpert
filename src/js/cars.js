95ea301038166359486426473003bcc239fe156b
Зокиржон З. Пулатов <z.pulatov@crowe.local>
2023-02-23 14:21:27 +0500

    test checking

---
+++ src/js/cars.js
@@ -0,0 +1,70 @@
+var carsInfo = {
+  bentley: {
+    name: "Bentley Continental GT III",
+    type: "Премиум-класс",
+    
+    params: {
+      year: "2019",
+      mileage: "10833 км",
+      carcase: "Купе",
+      color: "Красный",
+      equipment: "Individual",
+      gear: "Полный",
+      engine: "6.0 л/ Бензин",
+      transmission: "Робот",
+      rudder: "Левый",
+      owners: "1 владелец",
+      pts: "Оригинал",
+      holded: "2 года 9месяцев",
+      customs: "Растаможен",
+      vin: "SCB**************",
+      condition: "Не требует ремонта",
+    },
+
+    topImages: {
+      mainImage: 'bentley1.png',
+      thumbnails: ['bentley2.jpg', 'bentley3.jpg', 'bentley4.jpg']
+    },
+
+    detailedView: {
+      description: "Примечание: заводская толщина лакокрасочного покрытия для автомобиля Bentley Continental GT III составляет 150–220 микрон (микрометров, мкм).",
+      slides: [
+        {
+          image: "bentley1",
+          text: "Толщина краски составляет 181 микрон, что свидетельствует о том, что крыша без окраса."
+        },
+        {
+          image: "bentley2",
+          text: "Толщина краски составляет 181 микрон, что свидетельствует о том, что крыша без окраса."
+        },
+        
+      ]
+    },
+
+    conslusion: {
+      title: "Заключение диагностики",
+      text: "Автомобиль Bentley Continental GT III оказался в хорошем состоянии. Окрашен капот и вся правая сторона. Сделано очень хорошо. Но из-за скола начал облазить лак у шильдика. По технически без нареканий, пробег оригинальный."
+    }
+  },
+
+  testCar: {
+    name: "test name",
+    type: "test 2",
+    params: {
+      year: "test 3",
+      mileage: "test 4",
+      carcase: "test 5",
+      color: "test 6",
+      equipment: "test 7",
+      engine: "test 8",
+      transmission: "test 9",
+      rudder: "test 10",
+      owners: "test 11",
+      pts: "test 12",
+      holded: "test 13",
+      customs: "test 14",
+      vin: "test 15",
+      condition: "test 16",
+    }
+  }
+}
---
+++ src/js/main.js
@@ -0,0 +1,76 @@
+document.addEventListener('DOMContentLoaded', () => {
+  tabSlider = new Swiper(".tabs-slider-js", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".tabs-slider__arrow--next",
+      prevEl: ".tabs-slider__arrow--prev",
+    },
+  });
+
+
+  const docWrapper = document.querySelector('body'),
+        popupModal = document.querySelector('.popup-modal'),
+        popupButton = document.querySelectorAll('.modal-btn-js'),
+        modalClose = popupModal.querySelector('.popup-modal__close'),
+        details = popupModal.querySelector('.details');
+
+  let imgUrl = '/static/images/';
+
+  popupModal.removeAttribute('style')
+
+  function openModal(docWrapper, popupModal) {
+    docWrapper.classList.add('overflow_h')
+    popupModal.classList.add('open')
+  }
+
+  function closeModal(docWrapper, popupModal) {
+    docWrapper.classList.remove('overflow_h')
+    popupModal.classList.remove('open')
+  }
+
+  popupButton.forEach(button => {
+    button.addEventListener('click', function () {
+      const carName = button.dataset.car,
+            selectedCar = carsInfo[carName]
+      details.querySelector('.details__name').innerText = selectedCar.name
+      details.querySelector('.details__type').innerText = selectedCar.type
+
+      if (selectedCar.topImages) {
+        const { mainImage, thumbnails } = selectedCar.topImages
+        const topImage = document.createElement('img')
+        topImage.setAttribute('src', `${imgUrl+mainImage}`)
+        details.querySelector('.details__image').appendChild(topImage)
+
+        thumbnails.forEach((thumbName) => {
+          let thumb = document.createElement('img')
+          thumb.setAttribute('src', `${imgUrl+thumbName}`)
+          details.querySelector('.details__thumbs').appendChild(thumb)
+        })
+      }
+      
+
+      const {params} = selectedCar
+      
+      for (key in params) {
+        details.querySelector(`.details__param [data-name="${key}"]`).innerText = params[key]
+      }
+
+      openModal(docWrapper, popupModal)
+    })
+  })
+
+  modalClose.addEventListener('click', function () {
+    closeModal(docWrapper, popupModal)
+    setTimeout( () => {
+      details.querySelector('.details__name').innerHTML = ''
+      details.querySelector('.details__type').innerHTML = ''
+      details.querySelector('.details__image').innerHTML = ''
+      details.querySelector('.details__thumbs').innerHTML = ''
+    }, 500)
+  })
+});
